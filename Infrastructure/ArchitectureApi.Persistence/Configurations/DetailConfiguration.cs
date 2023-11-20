32f0a1b819f7872d6664f24fb2f4f8e885908aa1
Şiar Sarıışık <siar_sar@hotmail.com>
2023-11-20 13:54:37 +0300

    EntityConfigurations

---
+++ Infrastructure/ArchitectureApi.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using ArchitectureApi.Domain.Entities;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ArchitectureApi.Persistence.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            Faker faker = new("tr");
+
+            Detail detail = new()
+            {
+                Id = 1,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 1,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+            Detail detail2 = new()
+            {
+                Id = 2,
+                Title = faker.Lorem.Sentence(2),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 3,
+                CreatedDate = DateTime.Now,
+                IsDeleted = true
+            };
+            Detail detail3 = new()
+            {
+                Id = 3,
+                Title = faker.Lorem.Sentence(1),
+                Description = faker.Lorem.Sentence(5),
+                CategoryId = 4,
+                CreatedDate = DateTime.Now,
+                IsDeleted = false
+            };
+
+            builder.HasData(detail, detail2, detail3);
+        }
+    }
+}
