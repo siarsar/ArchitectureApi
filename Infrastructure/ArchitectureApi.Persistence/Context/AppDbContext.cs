3291136b131b23a365ca7a337911217b74df1004
Şiar Sarıışık <siar_sar@hotmail.com>
2023-11-22 16:47:43 +0300

    addMigrations

---
+++ Infrastructure/ArchitectureApi.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ArchitectureApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ArchitectureApi.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Detail> Details { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+        }
+    }
+}
